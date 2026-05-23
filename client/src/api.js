fcf156179469c880ec96213b183d8a0d2aac484b
awaiscodev <awaiscodev.pk@gmail.com>
2026-05-23 22:35:39 +0500

    Fix live API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://pet-insurance-virid.vercel.app/api",
 });
 
 export default api;
